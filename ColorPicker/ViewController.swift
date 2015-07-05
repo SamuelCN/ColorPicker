536c08b8f69d8fcda81ee3af512e1d91984a9c5c
Samuel C <samuelc@zipa.net.au>
2015-07-06 09:45:40 +1000

    Deployment Changes

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var ColorPicker: UIPickerView!
+    
+    var pickerColors:NSMutableArray!
+    var R:CGFloat = 255
+    var G:CGFloat = 255
+    var B:CGFloat = 255
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ColorPicker.delegate = self
+        ColorPicker.dataSource = self
+        
+        pickerColors = []
+        
+        for i in 0...255
+        {
+            pickerColors.addObject("\(255-i)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Data Sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerColors.count
+    }
+    
+    //MARK: Delegates
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerColors[row] as! String
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.view.backgroundColor = GetColor(pickerColors[row] as! String, colorValue: component)
+    }
+    
+    func GetColor(colorName:String, colorValue:Int) -> UIColor {
+        switch(colorValue) {
+            case 0:
+                R = StringToFloat(colorName)
+            case 1:
+                G = StringToFloat(colorName)
+            case 2:
+                B = StringToFloat(colorName)
+            default:
+                R = StringToFloat("0")
+            
+        }
+        
+        return UIColor(red: R/255, green: G/255, blue: B/255, alpha: 1)
+    }
 
+
+    func StringToFloat(value:String) -> CGFloat {
+        if let n = NSNumberFormatter().numberFromString(value) {
+            let f = CGFloat(n)
+            return f
+        }
+        
+        return CGFloat(0)
+    }
 }
 
