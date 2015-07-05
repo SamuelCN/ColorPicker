4ac2fdd4f2b5d7379d94add6577eb11471ac1008
Samuel C <samuelc@zipa.net.au>
2015-07-05 12:42:51 +1000

    Initial Commit

---
+++ ColorPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by Samuel C on 5/07/2015.
+//  Copyright (c) 2015 Zipa Smart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
